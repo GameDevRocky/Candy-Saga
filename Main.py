2b95286c2c469cfec989d91e1f6a114627f8418d
GameDevRocky <rocklyn.clarke1@gmail.com>
2024-10-11 21:54:43 -0400

    Made a tile editor

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
 import pygame 
+import sys
+from TileEditor import*
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+manager = Manager('Level Data')
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    clock.tick(FPS)
